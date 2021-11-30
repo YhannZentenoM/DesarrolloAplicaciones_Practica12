c36711cfb09f2455cb9e10ea9d426e179ecd1b5d
polaco300 <35962233+polaco300@users.noreply.github.com>
2021-11-30 10:06:22 -0500

    Add files via upload

---
+++ propuesto1/Ajpagina1.php
@@ -0,0 +1,84 @@
+<?PHP 
+    switch ($_REQUEST['cod']) {
+        case '1':
+            echo "<table>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Imagen</th>
+            </tr>
+            <tr>
+                <td>PC GAMER Core I3-10TH</td>
+                <td>10ma RAM 16GB DISCO 1TB VIDEO GT 4GB CASE 500W LED</td>
+                <td>S/ 2,669.00</td>
+                <td><img src='images/1.jpg' width='100'></td>
+            </tr>
+            </table";
+            break;
+        case '2':
+            echo "<table>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Imagen</th>
+            </tr>
+            <tr>
+                <td>Play Station 5</td>
+                <td>Consola Ps5 Lector Disco + Demon Souls</td>
+                <td>S/ 5,499.00</td>
+                <td><img src='images/2.jpg' width='100'></td>
+            </tr>
+            </table";
+            break;
+        case '3':
+            echo "<table>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Imagen</th>
+            </tr>
+            <tr>
+                <td>ORDRO HDV-F5 1080P</td>
+                <td>Full HD 3.0 Pantalla táctil giratoria LCD Cámara de video digital Grabadora Videocámara</td>
+                <td>S/ 430.00</td>
+                <td><img src='images/3.jpg' width='100'></td>
+            </tr>
+            </table";
+            break;
+        case '4':
+            echo "<table>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Imagen</th>
+            </tr>
+            <tr>
+                <td>Laptop Apple Macbook</td>
+                <td>Air M1 256GB 8 Ram - Space Gray/Silver/Gold</td>
+                <td>S/ 4,599.00</td>
+                <td><img src='images/4.jpg' width='100'></td>
+            </tr>
+            </table";
+            break;
+        case '5':
+            echo "<table>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Imagen</th>
+            </tr>
+            <tr>
+                <td>HP DeskJet Ink</td>
+                <td>Impresora Multifuncional HP DeskJet Ink Advantage 2775</td>
+                <td>S/ 349.00</td>
+                <td><img src='images/5.jpg' width='100'></td>
+            </tr>
+            </table";
+            break;
+    }
+?>
